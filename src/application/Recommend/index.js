e0edf0f1636d6cd06066f9cd0647ebb1868a0cd2
tsai <mengjietsai@163.com>
2021-02-18 18:34:49 +0800

    swiper组件的封装

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,9 +1,12 @@
-import React, { memo } from 'react'
-
+import React, { memo } from 'react';
+import Slider from '@/components/Slider';
 export default memo(function Recommend() {
-  return (
-    <div>
-      Recommend
-    </div>
-  )
-})
+  //mock 数据
+  const bannerList = [1, 2, 3, 4].map((item) => {
+    return {
+      imageUrl:
+        'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg',
+    };
+  });
+  return <Slider bannerList={bannerList}></Slider>;
+});
