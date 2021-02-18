2ae01831ba898fa9468c1ef22635246716643e11
tsai <mengjietsai@163.com>
2021-02-19 00:29:43 +0800

    Scroll基本封装完成

--- src/baseUI/Scroll/index.js
+++ src/baseUI/Scroll/index.js
@@ -1,4 +1,11 @@
-import React, { forwardRef, memo } from 'react';
+import React, {
+  forwardRef,
+  memo,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import PropTypes from 'prop-types';
 import BScroll from '@better-scroll/core';
 import styled from 'styled-components';
@@ -8,8 +15,101 @@ const ScrollContainer = styled.div`
   overflow: hidden;
 `;
 const Scroll = forwardRef((props, ref) => {
-  
-  return <ScrollContainer></ScrollContainer>;
+  const {
+    direction,
+    click,
+    refresh,
+    pullUpLoading,
+    pullDownLoading,
+    bounceTop,
+    bounceBottom,
+  } = props;
+  const { pullUp, pullDown, onScroll } = props;
+  //better-scroll 实例对象
+  const [bScroll, setBScroll] = useState(null);
+  //current 指向初始化 bs 实例需要的 DOM 元素
+  const scrollContaninerRef = useRef(null);
+  useEffect(() => {
+    _initScroll();
+    return () => {
+      setBScroll(null);
+    };
+    //eslint-disable-next-line
+  }, []);
+  // 如果bScroll实例不为空且传入onScroll 为bScroll绑定事件
+  useEffect(() => {
+    if (!bScroll || !onScroll) return;
+    bScroll.on('scroll', (scroll) => {
+      onScroll(scroll);
+    });
+    return () => {
+      bScroll.off('scroll');
+    };
+  }, [onScroll, bScroll]);
+  // 如果bScroll实例不为空且传入pullUp 为bScroll绑定事件
+  useEffect(() => {
+    if (!bScroll || !pullUp) return;
+    bScroll.on('scrollEnd', () => {
+      // 判断是否滑动到了底部
+      if (bScroll.y <= bScroll.maxScrollY + 100) {
+        pullUp();
+      }
+    });
+    return () => {
+      bScroll.off('scrollEnd');
+    };
+  }, [pullUp, bScroll]);
+  // 如果bScroll实例不为空且传入pullDown 为bScroll绑定事件
+  useEffect(() => {
+    if (!bScroll || !pullDown) return;
+    bScroll.on('touchEnd', (pos) => {
+      // 判断用户的下拉动作
+      if (pos.y > 50) {
+        pullDown();
+      }
+    });
+    return () => {
+      bScroll.off('touchEnd');
+    };
+  }, [pullDown, bScroll]);
+  // 每次重新渲染都要刷新实例，防止无法滑动:
+  useEffect(() => {
+    if (refresh && bScroll) {
+      bScroll.refresh();
+    }
+  });
+  useImperativeHandle(ref, () => ({
+    refresh() {
+      if (bScroll) {
+        bScroll.refresh();
+        bScroll.scrollTo(0, 0);
+      }
+    },
+    getBScroll() {
+      if (bScroll) {
+        return bScroll;
+      }
+    },
+  }));
+  const _initScroll = () => {
+    const scroll = new BScroll(scrollContaninerRef.current, {
+      scrollX: direction === 'horizontal',
+      scrollY: direction === 'vertical',
+      probeType: 3,
+      click: click,
+      bounce: {
+        top: bounceTop,
+        bottom: bounceBottom,
+      },
+    });
+    setBScroll(scroll);
+  };
+
+  return (
+    <ScrollContainer className="scroll" ref={scrollContaninerRef}>
+      {props.children}
+    </ScrollContainer>
+  );
 });
 
 Scroll.defaultProps = {
