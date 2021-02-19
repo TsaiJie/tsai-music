3bdafcce4d44651e175699bf28e0d739aaa34684
tsai <mengjietsai@163.com>
2021-02-19 12:17:07 +0800

    歌手列表的结构

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,19 +1,11 @@
 import React, { memo, useCallback, useState } from 'react';
 import { categoryTypes, alphaTypes } from '@/api/config';
 import HorizonScroll from '@/baseUI/HorizonScroll';
-import styled from 'styled-components';
-import style from '@/assets/global-style';
-export const NavContainer = styled.div`
-  box-sizing: border-box;
-  position: fixed;
-  top: 95px;
-  width: 100%;
-  padding: 5px;
-  overflow: hidden;
-`;
+import { NavContainer, ListItem, List, ListContainer } from './style';
+import Scroll from '@/baseUI/Scroll';
 export default memo(function Singers() {
-  const [category, setCategory] = useState('');
-  const [alpha, setAlpha] = useState('');
+  const [category, setCategory] = useState({});
+  const [alpha, setAlpha] = useState({});
   const handleUpdateAlpha = useCallback((val) => {
     console.log(val);
     setAlpha(val);
@@ -23,20 +15,56 @@ export default memo(function Singers() {
     console.log(val);
     setCategory(val);
   }, []);
+  //mock 数据
+  const singerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].map((item) => {
+    return {
+      picUrl:
+        'https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg',
+      name: '隔壁老樊',
+      accountId: 277313426,
+    };
+  });
+  // 渲染函数，返回歌手列表
+  const renderSingerList = () => {
+    return (
+      <List>
+        {singerList.map((item, index) => {
+          return (
+            <ListItem>
+              <div className="img_wrapper">
+                <img
+                  src={`${item.picUrl}?param=300x300`}
+                  width="100%"
+                  height="100%"
+                  alt="music"
+                />
+              </div>
+              <span className="name">{item.name}</span>
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+  };
   return (
-    <NavContainer>
-      <HorizonScroll
-        list={categoryTypes}
-        title={'分类 (默认热门):'}
-        handleClick={handleUpdateCatetory}
-        value={category.key}
-      />
-      <HorizonScroll
-        list={alphaTypes}
-        title={'首字母:'}
-        handleClick={handleUpdateAlpha}
-        value={alpha.key}
-      />
-    </NavContainer>
+    <div>
+      <NavContainer>
+        <HorizonScroll
+          list={categoryTypes}
+          title={'分类 (默认热门):'}
+          handleClick={handleUpdateCatetory}
+          value={category.key}
+        />
+        <HorizonScroll
+          list={alphaTypes}
+          title={'首字母:'}
+          handleClick={handleUpdateAlpha}
+          value={alpha.key}
+        />
+      </NavContainer>
+      <ListContainer>
+        <Scroll data={singerList}>{renderSingerList()}</Scroll>
+      </ListContainer>
+    </div>
   );
 });
---
+++ src/application/Singers/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+export const NavContainer = styled.div`
+  box-sizing: border-box;
+  position: fixed;
+  top: 95px;
+  width: 100%;
+  padding: 5px;
+  overflow: hidden;
+`;
+export const ListContainer = styled.div``;
+export const List = styled.div``;
+export const ListItem = styled.div``;
