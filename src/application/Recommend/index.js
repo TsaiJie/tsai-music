12a9dfe552a63b750fb0bc65e7e26bdf47ecfe7b
tsai <mengjietsai@163.com>
2021-02-19 10:54:47 +0800

    loading组件的使用

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -6,21 +6,25 @@ import RecommendList from '@/components/RecommendList';
 import Scroll from '@/baseUI/Scroll';
 import { Content } from './style';
 import {
+  changeEnterLoading,
   getBannerListAction,
   getRecommendListAction,
 } from './store/actionCreators';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import Loading from '@/baseUI/Loading';
 export default memo(function Recommend() {
   //mock 数据
   const dispatch = useDispatch();
-  const { bannerList, recommendList } = useSelector(
+  const { bannerList, recommendList, enterLoading } = useSelector(
     (state) => ({
       bannerList: state.recommend.bannerList,
       recommendList: state.recommend.recommendList,
+      enterLoading: state.recommend.enterLoading,
     }),
     shallowEqual
   );
   useEffect(() => {
+    dispatch(changeEnterLoading(true));
     dispatch(getBannerListAction());
     dispatch(getRecommendListAction());
   }, [dispatch]);
@@ -33,6 +37,7 @@ export default memo(function Recommend() {
           <RecommendList recommendList={recommendList}></RecommendList>
         </div>
       </Scroll>
+      {enterLoading ? <Loading /> : null}
     </Content>
   );
 });
---
+++ src/baseUI/Loading/index.js
@@ -0,0 +1,41 @@
+import React, { memo } from 'react';
+import styled, { keyframes } from 'styled-components';
+import style from '../../assets/global-style';
+const loading = keyframes`
+  0%, 100% {
+    transform: scale(0.0);
+  }
+  50% {
+    transform: scale(1.0);
+  }
+`;
+
+const LoadingWrapper = styled.div`
+  > div {
+    position: fixed;
+    z-index: 1000;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    width: 60px;
+    height: 60px;
+    background: ${style['color-dialog-background']};
+    margin: auto;
+    opacity: 0.6;
+    -webkit-transform: rotate(0deg);
+    border-radius: 50%;
+    animation: ${loading} 1.4s infinite ease-in;
+  }
+  > div:nth-child(2) {
+    animation-delay: -0.7s;
+  }
+`;
+export default memo(function Loading() {
+  return (
+    <LoadingWrapper>
+      <div></div>
+      <div></div>
+    </LoadingWrapper>
+  );
+});
