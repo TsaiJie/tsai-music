8035e39ed430d0a4c25931b8a7a4d54af6ff5d45
tsai <mengjietsai@163.com>
2021-02-20 11:42:46 +0800

    rank基本结构

--- src/application/Rank/index.js
+++ src/application/Rank/index.js
@@ -1,7 +1,8 @@
+import { filterIndex } from '@/api/utils';
 import React, { memo, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRankListAction, changeLoadingAction } from './store';
-
+import { List, ListItem, Container, SongList } from './style';
 export default memo(function Rank() {
   const { rankList, loading } = useSelector((state) => ({
     rankList: state.rank.rankList,
@@ -14,6 +15,47 @@ export default memo(function Rank() {
       dispatch(getRankListAction());
     }
   }, [dispatch, rankList]);
-  console.log(rankList);
-  return <div>Rank</div>;
+  const globalStartIndex = filterIndex(rankList);
+  const officialList = rankList.slice(0, globalStartIndex);
+  const globalList = rankList.slice(globalStartIndex);
+  const enterDetail = (item) => {
+    console.log(item);
+  };
+  // 榜单数据未加载出来之前都给隐藏
+  let displayStyle = loading ? { display: 'none' } : { display: '' };
+
+  // 这是渲染榜单列表函数，传入 global 变量来区分不同的布局方式
+  const renderRankList = (list, global) => {
+    return (
+      <List>
+        {list.map((item) => {
+          return (
+            <ListItem
+              key={item.id}
+              tracks={item.tracks}
+              onClick={() => enterDetail(item)}
+            >
+              <div className="img_wrapper">
+                <img src={item.coverImgUrl} alt="排行" />
+                <div className="decorate"></div>
+                <span className="update_frequency">{item.updateFrequency}</span>
+              </div>
+            </ListItem>
+          );
+        })}
+      </List>
+    );
+  };
+  return (
+    <div>
+      <h1 className="offical" style={displayStyle}>
+        官方榜
+      </h1>
+      {renderRankList(officialList)}
+      <h1 className="global" style={displayStyle}>
+        全球榜
+      </h1>
+      {renderRankList(globalList)}
+    </div>
+  );
 });
---
+++ src/application/Rank/style.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+export const List = styled.ul``;
+export const ListItem = styled.li``;
+export const SongList = styled.ul``;
