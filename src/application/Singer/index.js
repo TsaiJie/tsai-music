554926f6d7173a2dbcbecb2907bb1a5da54079ee
tsai <mengjietsai@163.com>
2021-02-21 08:22:54 +0800

    进入singer动画

--- src/application/Singer/index.js
+++ src/application/Singer/index.js
@@ -1,7 +1,21 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import { Container } from './style';
 export default memo(function Singer(props) {
+  const [showStatus, setShowStatus] = useState(true);
+
   const { id } = props.match.params;
 
-  return <Container>Singer {id}</Container>;
+  return (
+    <CSSTransition
+      in={showStatus}
+      timeout={300}
+      classNames="fly"
+      appear={true}
+      unmountOnExit
+      onExited={() => props.history.goBack()}
+    >
+      <Container>Singer {id}</Container>
+    </CSSTransition>
+  );
 });
--- src/application/Singer/style.js
+++ src/application/Singer/style.js
@@ -3,10 +3,27 @@ import style from '@/assets/global-style';
 
 export const Container = styled.div`
   position: fixed;
-  top:0;
+  top: 0;
   bottom: 0;
   right: 0;
   left: 0;
   z-index: 100;
   background-color: ${style['color-background']};
+  overflow: hidden;
+  &.fly-enter,
+  &.fly-appear {
+    transform: rotateZ(30deg) translate3d(100%, 0, 0);
+  }
+  &.fly-enter-active,
+  &.fly-appear-active {
+    transition: transform 0.3s;
+    transform: rotateZ(0deg) translate3d(0, 0, 0);
+  }
+  &.fly-exit {
+    transform: rotateZ(0deg) translate3d(0, 0, 0);
+  }
+  &.fly-exit-active {
+    transition: transform 0.3s;
+    transform: rotateZ(30deg) translate3d(100%, 0, 0);
+  }
 `;
