7eb8e34f8d871e8bcbb8a1f3b04b2e0b2a4d9b92
tsai <mengjietsai@163.com>
2021-02-22 22:44:51 +0800

    ProgressBar的基本样式

--- src/baseUI/ProgressBar/index.js
+++ src/baseUI/ProgressBar/index.js
@@ -3,8 +3,49 @@ import styled from 'styled-components';
 import style from '@/assets/global-style';
 import { prefixStyle } from '@/api/utils';
 const ProgressBarWrapper = styled.div`
-
+  height: 30px;
+  .bar-inner {
+    position: relative;
+    top: 13px;
+    height: 4px;
+    -webkit-transform: rotate(0deg);
+    border-radius: 2px;
+    background: ${style['color-theme-d']};
+    .progress {
+      position: absolute;
+      height: 100%;
+      background: ${style['color-theme']};
+    }
+  }
+  .progress-btn-wrapper {
+    position: absolute;
+    left: -8px;
+    top: -13px;
+    width: 30px;
+    height: 30px;
+    .progress-btn {
+      box-sizing: border-box;
+      position: relative;
+      top: 7px;
+      left: 7px;
+      width: 16px;
+      height: 16px;
+      border: 3px solid ${style['border-color']};
+      -webkit-transform: rotate(0deg);
+      border-radius: 50%;
+      background: ${style['color-theme']};
+    }
+  }
 `;
 export default memo(function ProgressBar() {
-  return <ProgressBarWrapper>ProgressBarWrapper</ProgressBarWrapper>;
+  return (
+    <ProgressBarWrapper>
+      <div className="bar-inner">
+        <div className="progress"></div>
+        <div className="progress-btn-wrapper">
+          <div className="progress-btn"></div>
+        </div>
+      </div>
+    </ProgressBarWrapper>
+  );
 });
