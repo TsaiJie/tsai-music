d2794080e9887bf9d20de7374445c110f30e3774
tsai <mengjietsai@163.com>
2021-02-24 11:55:16 +0800

    playList大致样式1

--- src/application/Player/PlayList/style.js
+++ src/application/Player/PlayList/style.js
@@ -9,7 +9,24 @@ export const PlayListWrapper = styled.div`
   bottom: 0;
   z-index: 1000;
   background: ${style['color-background-d']};
+  .list_wrapper {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    opacity: 1;
+    border-radius: 10px 10px 0 0;
+    background: ${style['color-background']};
+    transform: translate3d(0, 0, 0);
+    .list_close {
+      text-align: center;
+      background: red;
+    }
+  }
 `;
-export const ScrollWrapper = styled.div``;
+export const ScrollWrapper = styled.div`
+  height: 400px;
+  overflow: hidden;
+`;
 export const ListHeader = styled.div``;
 export const ListContent = styled.ul``;
