f3e182ebcf8230a8f1aa513863037a4dc71040bf
tsai <mengjietsai@163.com>
2021-02-26 11:11:47 +0800

    搜索框进出动画，路由切换逻辑

--- src/application/Search/index.js
+++ src/application/Search/index.js
@@ -1,9 +1,25 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { Container } from './style';
 
-export default memo(function Search() {
+export default memo(function Search(props) {
+  const [show, setShow] = useState(false);
+  useEffect(() => {
+    setShow(true);
+  }, []);
   return (
-    <div>
-      Search
-    </div>
-  )
-})
+    <CSSTransition
+      in={show}
+      timeout={300}
+      appear={true}
+      classNames="fly"
+      unmountOnExit
+      mountOnEnter
+      onExited={() => props.history.goBack()}
+    >
+      <Container>
+        <div onClick={() => setShow(false)}> 返回 </div>Search
+      </Container>
+    </CSSTransition>
+  );
+});
